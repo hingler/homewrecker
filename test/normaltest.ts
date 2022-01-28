5bfa1719f63f0a4d0d45b203b5f6f43c1e4b591a
jamieboy1337 <jamieboy1337@gmail.com>
2022-01-28 01:20:44 -0800

    normals check out

---
+++ test/normaltest.ts
@@ -0,0 +1,62 @@
+import { expect } from "chai";
+import { vec3 } from "gl-matrix";
+import { RoofNormalGenerator } from "../ts/model/internal/RoofNormalGenerator";
+import { RoofPositionGenerator } from "../ts/model/internal/RoofPositionGenerator";
+import { Segment } from "../ts/segment/Segment";
+
+
+describe("RoofNormalGenerator", function() {
+  it("Generates proper length results", function() {
+    const testSegment : Segment = {
+      start: [0, 0],
+      end: [10, 0],
+      flat: false,
+      startJoin: false
+    };
+
+    const normData = RoofNormalGenerator.generateRoofNormals(testSegment, 0, 5);
+
+    expect(normData.longMinus.length).to.equal(3);
+    expect(normData.longPlus.length).to.equal(3);
+    expect(normData.shortStart.length).to.equal(3);
+    expect(normData.shortEnd.length).to.equal(3);
+  });
+
+  it("Returns normals that are close enough to cross products", function() {
+    const testSegment : Segment = {
+      start: [0, 0],
+      end: [10, 0],
+      flat: false,
+      startJoin: false
+    };
+    for (let g = 0.5; g < 15; g += 0.5) {
+      const posData = RoofPositionGenerator.generateRoofPositions(testSegment, g, 3);
+      const normData = RoofNormalGenerator.generateRoofNormals(testSegment, g, 3);
+  
+      const pos = [posData.longMinus, posData.longPlus, posData.shortStart, posData.shortEnd];
+      const norm = [normData.longMinus, normData.longPlus, normData.shortStart, normData.shortEnd];
+      const temp = vec3.create();
+      const temp2 = vec3.create();
+      const temp3 = vec3.create();
+      for (let h = 0; h < 4; h++) {
+        let facePos = pos[h];
+        let faceNorm = norm[h];
+        for (let i = 0; i < 3; i++) {
+          temp[i] = facePos[i];
+          temp2[i] = facePos[3 + i];
+          temp3[i] = facePos[6 + i];
+        }
+  
+        vec3.sub(temp, temp, temp2);
+        vec3.sub(temp2, temp3, temp2);
+        vec3.cross(temp3, temp2, temp);
+        vec3.normalize(temp3, temp3);
+  
+        vec3.copy(temp2, faceNorm as [number, number, number]);
+        for (let i = 0; i < 3; i++) {
+          expect(temp3[i]).to.approximately(temp2[i], 0.0001);
+        }
+      }
+    }
+  });
+});
---
+++ ts/model/internal/RoofNormalGenerator.ts
@@ -0,0 +1,68 @@
+import { vec2, vec3 } from "gl-matrix";
+import { Segment } from "ts/segment/Segment";
+import { RoofPolyData } from "./RoofPolyData";
+
+export class RoofNormalGenerator {
+  /**
+   * Generates normal data for a roof.
+   * Since all faces share a single normal, this will only output one vec3 for each one.
+   * @param segment - segment being generated.
+   * @param height - height of roof at peak (edge is always at 0)
+   * @param extrude - distance from segment to extrude roof.
+   */
+  static generateRoofNormals(segment: Segment, height: number, extrude: number) : RoofPolyData {
+    const startRef = vec3.fromValues(segment.start[0], 0.0, segment.start[1]);
+    const endRef = vec3.fromValues(segment.end[0], 0.0, segment.end[1]);
+    const dir = vec3.subtract(vec3.create(), endRef, startRef);
+    vec3.normalize(dir, dir);
+
+    const norm = vec3.cross(vec3.create(), dir, [0, 1, 0]);
+    vec3.normalize(norm, norm);
+
+    const raw = vec2.create();
+    raw[0] = height;
+    raw[1] = extrude;
+    vec2.normalize(raw, raw);
+
+    let res = {} as RoofPolyData;
+    res.longPlus = [];
+    res.longMinus = [];
+    res.shortEnd = null;
+    res.shortStart = null;
+
+    const temp = vec3.create();
+    const temp2 = vec3.create();
+
+    // -long
+    vec3.add(temp, vec3.zero(temp), vec3.scale(temp2, norm, -raw[0]));
+    temp[1] += raw[1];
+    vec3.normalize(temp, temp);
+    res.longMinus.push(...temp);
+
+    // +long
+    vec3.add(temp, vec3.zero(temp), vec3.scale(temp2, norm, raw[0]));
+    temp[1] += raw[1];
+    vec3.normalize(temp, temp);
+    res.longPlus.push(...temp);
+
+    // startshort
+
+    if (!segment.flat) {
+      if (!segment.startJoin) {
+        vec3.add(temp, vec3.zero(temp), vec3.scale(temp2, dir, -raw[0]));
+        temp[1] += raw[1];
+        res.shortStart = [];
+        vec3.normalize(temp, temp);
+        res.shortStart.push(...temp);
+      }
+
+      vec3.add(temp, vec3.zero(temp), vec3.scale(temp2, dir, raw[0]));
+      temp[1] += raw[1];
+      res.shortEnd = [];
+      vec3.normalize(temp, temp);
+      res.shortEnd.push(...temp);
+    }
+
+    return res;
+  }
+}
