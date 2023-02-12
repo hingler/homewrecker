4d95f574c2ef7b5f435d8d5f954a9d7383695695
jamieboy1337 <jamieboy1337@gmail.com>
2023-02-11 21:52:34 -0500

    resolve random seeding

--- ts/model/SegmentGenerator.ts
+++ ts/model/SegmentGenerator.ts
@@ -2,25 +2,18 @@ import { vec2 } from "gl-matrix";
 import { Segment } from "../segment/Segment";
 import { distanceBetweenLines } from "./internal/distanceBetweenLines";
 import { generateDirNorm } from "./internal/generateDirNorm";
+import { Random } from "random"
+import seedrandom from "seedrandom"
 
 export interface ProceduralSegment extends Segment {
   // reference to a parent segment's number, or -1
   parent: number;
 }
 
-function xorshift32() {
-  return 128;
-}
-
-function xorshift32_seed(seed: number) {}
-
-function xorshift32_float() {
-  return 0.45;
-}
-
 const EDGE_BRANCH_PROBABILITY = 0.125;
 
 export class SegmentGenerator {
+
   private static getSegmentLength(seg: Segment) {
     const len = vec2.create();
     vec2.sub(len, seg.end, seg.start);
@@ -28,18 +21,21 @@ export class SegmentGenerator {
   }
 
   static generateSegments(extrude: number, seed?: number) {
+    let random: Random;
     if (seed !== undefined) {
-      xorshift32_seed(seed);
-    }
+      random = new Random(seedrandom(seed))
+    } else {
+      random = new Random()
+    } 
 
     // create a random initial segment
     // center it and have it sit on an axis
 
 
     // 2.5 - 10.0
-    const length = 6.0 + xorshift32_float() * 5.0;
+    const length = 6.0 + random.float(0.0, 1.0) * 5.0;
     // pull int values from this
-    const seed_axis = xorshift32();
+    const seed_axis = random.integer();
 
     // pull higher ord bits
     const axisHoriz = (seed_axis & 8192 ? 1 : 0);
@@ -63,18 +59,18 @@ export class SegmentGenerator {
     // - if there are any parallel segments which also branch off of that wing, skip
     // - determine an arbitrary length (let's cap it at the length of the base segment)
 
-    const wingCount = Math.floor(3.0 * xorshift32_float() + 1.0);
+    const wingCount = Math.floor(3.0 * random.float(0.0, 1.0) + 1.0);
     for (let i = 0; i < wingCount; i++) {
       let placeSegment = false;
       let tries = 0;
       while (!placeSegment && tries < 24) {
         placeSegment = true;
-        const index = Math.abs(xorshift32() >>> 12) % segmentList.length;
+        const index = Math.abs(random.integer() >>> 12) % segmentList.length;
         // pick random parent
         const parent = segmentList[index];
   
         // boost the probability slightly of branching off the ends
-        const branchPos = Math.min(1.0, Math.max(xorshift32_float() * (1.0 + EDGE_BRANCH_PROBABILITY) - 0.5 * EDGE_BRANCH_PROBABILITY, 0.0));
+        const branchPos = Math.min(1.0, Math.max(random.float(0.0, 1.0) * (1.0 + EDGE_BRANCH_PROBABILITY) - 0.5 * EDGE_BRANCH_PROBABILITY, 0.0));
         const startPoint = vec2.create();
   
         // look at parent's params
@@ -86,12 +82,12 @@ export class SegmentGenerator {
         vec2.copy(startPoint, parent.start);
         // startpoint is somewhere along the line
         vec2.scaleAndAdd(startPoint, startPoint, dir2, branchPos);
-        const flip = Math.round(xorshift32_float()) * 2 - 1;
+        const flip = Math.round(random.float(0.0, 1.0)) * 2 - 1;
         // norm2 is in one of the orthogonal directions
         vec2.scale(norm2, norm2, flip);      
         
         
-        const childLength = Math.max(3.5, extrude + 0.5) + xorshift32_float() * 4.5;
+        const childLength = Math.max(3.5, extrude + 0.5) + random.float(0.0, 1.0) * 4.5;
         vec2.scale(norm2, norm2, childLength);
         const segChild : ProceduralSegment = {
           start: startPoint,
